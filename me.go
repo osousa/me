4b0ca79cd2d6fde7d53405178f17dc3fa80bc380
osousa <osousa@protonmail.com>
2022-03-12 12:32:45 +0000

    ignore

--- me.go
+++ me.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-    "fmt"
     "net/http"
+    "flag"
+    "fmt"
 )
+
+func version() string{
+    version := "0.1";
+    return version
+}
+
 
 //1--little comment
 func HelloWorld(res http.ResponseWriter, req *http.Request) {
@@ -11,6 +18,14 @@ func HelloWorld(res http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+    versionPtr := flag.Bool("version", false, "versioning")
+    flag.Parse()
+
+    if *versionPtr != false {
+        fmt.Println(version())
+        return
+    }
+
     http.HandleFunc("/", HelloWorld)
     http.ListenAndServe(":3000", nil)
 }
